1ba24f62b7a3a7d9fc4b74389509b1f586fe0cc6
Daniel Barbour <daniel.devnull@gmail.com>
2018-07-05 22:21:54 -0500

    Add dropdown, functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit, ElementRef} from '@angular/core';
+import * as faker from 'faker';
+export interface DropdownData {
+  image: string;
+  description: string;
+}
+export interface DropdownElement extends Element {
+  toggleDropdown: () => {};
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  dropdownData: DropdownData[] = [];
+  dropdownRef: DropdownElement;
+  selectedItem: DropdownData;
+  constructor(private elem: ElementRef) {}
+  createData(numItems: number): void {
+    for (let i = 0; i < numItems; i++) {
+      this.dropdownData.push({
+        image: 'https://picsum.photos/720?' + i,
+        description: faker.lorem.sentences(1,3)
+      });
+    }
+  }
+  toggleDropdown(index: number) {
+    this.dropdownRef.toggleDropdown();
+    this.selectedItem = this.dropdownData[index];
+    console.log(this.selectedItem);
+  }
+  handleCallback(event) {
+    console.log(event);
+  }
+  ngOnInit() {
+    this.createData(3);
+    this.dropdownRef = this.elem.nativeElement.querySelector('ui-dropdown#uniqueDropdown');
+  }
 }
